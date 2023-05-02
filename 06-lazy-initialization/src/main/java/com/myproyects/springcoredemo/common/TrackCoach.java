5f0fc71a66e8f6aa1b42d0fcb145e022dbbb7f36
HugoSanche <hbmclaren@gmail.com>
2023-05-01 22:27:12 -0600

    Working with lazy initialization

---
+++ 06-lazy-initialization/src/main/java/com/myproyects/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,16 @@
+package com.myproyects.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy //By default Spring start all class buy if you put @Lazy this class not start by default
+public class TrackCoach implements Coach{
+    public TrackCoach(){
+        System.out.println("In constructor: "+getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Run 10 kilometers";
+    }
+}
---
+++ 06-lazy-initialization/src/main/java/com/myproyects/springcoredemo/rest/DemoController.java
@@ -0,0 +1,32 @@
+package com.myproyects.springcoredemo.rest;
+
+import com.myproyects.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//NOTA.- Spring injection types
+//A) Recommended by the spring.io development io
+// 1.-Constructor injection. Required dependencies
+// 2.-Setter injection. Optional dependencies
+
+//B)Not recommend by the spring.io develop team
+// 1.-Field injection
+
+@RestController
+public class DemoController {//constructor injection
+    //define private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    //Nota.- @Qualifier("tennisCoach" first letter ins lowercase for the class TennisCoach
+  public DemoController(@Qualifier("cricketCoach") Coach thecoach){
+        System.out.println("In constructor: "+getClass().getSimpleName());
+        myCoach=thecoach;
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
